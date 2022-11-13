68c69ea788b0fc4c63e56a8bf7682f46ecd2c278
Suhas44 <suhas8@outlook.com>
2022-11-13 14:42:00 -0600

    Setup

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Homepage = () => {
+  return (
+    <div style={{margin: '2rem'}}>
+      <div>
+      <h1>Hi, I'm Suhas!</h1> 
+      <p>I'm a developer at Axo, Texas Torque, and Extendable.</p>
+
+      <p>Feel free to reach out to me on the following platforms:</p>
+        <ul>
+            <li><a href="
+https://www.linkedin.com/in/suhas-guddeti-1b45a7247/">LinkedIn</a></li>
+            <li><a href="
+                https://github.com/Suhas44">GitHub</a></li>
+            <li><a href="
+                https://www.instagram.com/suhas.guddeti/">Instagram</a></li>
+        </ul>
+      </div>
+
+    </div>
+  );
+};
+
+export default Homepage;
